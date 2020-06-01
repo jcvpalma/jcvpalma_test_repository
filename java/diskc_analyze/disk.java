07701cc78e380277488eff47b51521a846dc607c
Julio Cesar Viana Palma <juliocvp@unicid.edu.br>
2020-06-01 10:30:22 -0300

    inclusao de novos algoritmos

---
+++ java/diskc_analyze/disk.java
@@ -0,0 +1,32 @@
+public class disk {
+    
+    public static void main(String[] args){
+
+        int x = 2;
+        int n = 3;
+        List<Integer> space = new ArrayList<Integer>();
+        space = fillSpace();
+
+        //Neste ponto eu devo "splitar" o array em pedaços e verificar as divisões
+        //e.g.: [2,3,4,5] com digito de separacao de 2 => [2,3],[3,4],[4,5] => 3 e 4 repetem, 
+        //entao retorna o maximo do minimo
+
+
+        
+
+
+
+    }
+
+    public static List<Integer> fillSpace(){
+        List<Integer> space = new ArrayList<Integer>();
+        space.add(8);
+        space.add(2);
+        space.add(4);
+        //space.add(1);
+       // space.add(2);
+        return sapce;
+    }
+
+
+}
